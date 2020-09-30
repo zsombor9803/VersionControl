03784cbdb7d974bed7a1a41e415f2a63b2cedfad
Zsombor Nyikus <zsombor.nyikus@stud.uni-corvinus.hu>
2020-09-30 16:33:08 +0200

    commit

--- gyak004/gyak004/Form1.cs
+++ gyak004/gyak004/Form1.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
 
 namespace gyak004
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
         }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList(); 
+        }
+
     }
 }

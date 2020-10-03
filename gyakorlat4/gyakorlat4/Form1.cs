759e719ce89b3f552bdc169845bdd69efd204866
Zsombor Nyikus <zsombor.nyikus@stud.uni-corvinus.hu>
2020-10-03 14:11:06 +0200

    commit

---
+++ gyakorlat4/gyakorlat4/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gyakorlat4
+{
+    public partial class Form1 : Form
+    {
+        List<Flat> Flats;
+        RealEstateEntities context = new RealEstateEntities();
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
+        }
+    }
+}

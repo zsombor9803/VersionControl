36d1de94c7a4306515953fb38c40b4e14f5977f5
Zsombor Nyikus <zsombor.nyikus@stud.uni-corvinus.hu>
2020-10-03 14:33:37 +0200

    commit

--- gyakorlat4/gyakorlat4/Form1.cs
+++ gyakorlat4/gyakorlat4/Form1.cs
@@ -16,15 +16,50 @@ namespace gyakorlat4
     {
         List<Flat> Flats;
         RealEstateEntities context = new RealEstateEntities();
+
+        Excel.Application xlApp; //Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; //létrehozott munkafüzet
+        Excel.Worksheet xlSheet; //Munkalap a munkafüzeten belül
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         private void LoadData()
         {
             Flats = context.Flats.ToList();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                
+                xlApp = new Excel.Application(); // Excel elindítása és az applikáció objektum betöltése
+               
+                xlWB = xlApp.Workbooks.Add(Missing.Value); // Új munkafüzet
+
+                xlSheet = xlWB.ActiveSheet; // Új munkalap 
+
+                //CreateTable();
+
+                //Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                //Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
     }
 }

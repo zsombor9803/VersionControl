c075be73fe7000ddc6789f33604b78b5808183ea
Zsombor Nyikus <zsombor.nyikus@stud.uni-corvinus.hu>
2020-10-07 14:06:58 +0200

    commit

--- gyak5/gyak5/Form1.cs
+++ gyak5/gyak5/Form1.cs
@@ -12,9 +12,13 @@ namespace gyak5
 {
     public partial class Form1 : Form
     {
+        List<Tick> Ticks;
+        PortfolioEntities context = new PortfolioEntities();
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }

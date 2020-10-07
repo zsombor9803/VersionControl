f31bcaeb227d8d6315a594340434649e75a94a34
Zsombor Nyikus <zsombor.nyikus@stud.uni-corvinus.hu>
2020-10-07 14:44:10 +0200

    commit

--- gyak5/gyak5/Form1.Designer.cs
+++ gyak5/gyak5/Form1.Designer.cs
@@ -29,7 +29,9 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -41,15 +43,27 @@
             this.dataGridView1.Size = new System.Drawing.Size(240, 150);
             this.dataGridView1.TabIndex = 0;
             // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(44, 249);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView2.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,6 +71,7 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
 
--- gyak5/gyak5/Form1.cs
+++ gyak5/gyak5/Form1.cs
@@ -14,7 +14,7 @@ namespace gyak5
     {
         List<Tick> Ticks;
         PortfolioEntities context = new PortfolioEntities();
-        List<Entities.PortfolioItem> Portfolios = new List<Entities.PortfolioItem>();
+        List<Entities.PortfolioItem> Portfolios = new List<Entities.PortfolioItem>(); //csak Entities.-al működik
         
         public Form1()
         {
@@ -22,6 +22,18 @@ namespace gyak5
             
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            //PortfolioItem p = new PortfolioItem() és p.Index = "OTP" stb... egyszerűbb formában való megadása
+            Portfolios.Add(new Entities.PortfolioItem() { Index = "OTP", Volume = 10 }); 
+            Portfolios.Add(new Entities.PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolios.Add(new Entities.PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolios;
         }
     }
 }

5f613d0af5d910812bfb8cb213dce6ff27e25937
Zsombor Nyikus <zsombor.nyikus@stud.uni-corvinus.hu>
2020-10-16 18:47:25 +0200

    commit

--- gyak6/gyak6/Form1.cs
+++ gyak6/gyak6/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak6.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,13 +13,24 @@ namespace gyak6
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            
         }
         private void GetExchangeRates()
         {
+            var mnbService = new MNBArfolyamServiceSoapClient();
 
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR", startDate = "2020-01-01", endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }

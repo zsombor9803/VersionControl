0c261c8bee69cb15f648603009a081ebb4431664
Zsombor Nyikus <zsombor.nyikus@stud.uni-corvinus.hu>
2020-10-25 12:14:44 +0100

    commit

--- gyak07/gyak07/Form1.cs
+++ gyak07/gyak07/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using gyak07.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +14,33 @@ namespace gyak07
 {
     public partial class Form1 : Form
     {
+        List<Person> Population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> deathProbabilities = new List<DeathProbability>();
+
         public Form1()
         {
             InitializeComponent();
-            
+           
+        }
+        public List<Person> GetPopulation(string csvpath)
+        {
+            List<Person> population = new List<Person>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    population.Add(new Person()
+                    {
+                        BirthYear = int.Parse(line[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        NumberOfChildren = byte.Parse(line[2])
+                    });
+                }
+            }
+            return population;
         }
     }
 }

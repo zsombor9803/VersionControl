c5a36d450391a54125969d45737c5c7e8432d1fd
Zsombor Nyikus <zsombor.nyikus@stud.uni-corvinus.hu>
2020-10-25 12:40:50 +0100

    commit

--- gyak07/gyak07/Form1.cs
+++ gyak07/gyak07/Form1.cs
@@ -14,6 +14,8 @@ namespace gyak07
 {
     public partial class Form1 : Form
     {
+        Random rnd = new Random(1234);
+
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> deathProbabilities = new List<DeathProbability>();

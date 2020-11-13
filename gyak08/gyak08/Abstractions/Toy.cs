ef38707faccacb22a33b1c2da2197dce67217607
Zsombor Nyikus <zsombor.nyikus@stud.uni-corvinus.hu>
2020-11-13 16:06:44 +0100

    commit

---
+++ gyak08/gyak08/Abstractions/Toy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gyak08.Abstractions
+{
+    public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Height = 50;
+            Width = 50;
+            Paint += Toy_Paint; 
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected abstract void DrawImage(Graphics g);
+        
+
+        public void MoveToy()
+        {
+            Left += 1;
+        }
+    }
+}
